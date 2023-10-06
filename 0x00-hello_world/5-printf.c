f803ac7e3f6931eceb6a343e5e1225c073015201
O-Omarr <omar.ahmed2666@gmail.com>
2023-10-06 04:38:29 -0700

    4

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a c program that uses printf funtion
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
