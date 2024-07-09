ca2c26dd59dfdd01a80ffb56f1e50e9827e8945b
O-Omarr <omar.ahmed2666@gmail.com>
2024-07-09 13:22:28 -0700

    ha

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
   * set_string - main function
@@ -9,10 +10,13 @@
   *
   * Return: Always 0.
   */
-void set_string(char **s, char *to)
-
+int main(void)
 {
-	*s = to;
+	char *s0 = "Bob Dylan";
+	char *s1 = "Robert Allen";
 
+	printf("%s, %s/n", s0, s1);
+	set_string(&s1, s0);
+	printf("%s, %s/n", s0, s1);
+	return (0);
 }
-
